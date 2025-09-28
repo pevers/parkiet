b47c01de3df125c700a6faeb95b9d9aed05298b3
Peter Evers <pevers90@gmail.com>
2025-09-28 15:04:06 +0200

    chore: commit save point

--- src/parkiet/jax/train_distributed.py
+++ src/parkiet/jax/train_distributed.py
@@ -1,4 +1,14 @@
-assert False, "This script is not used anymore"
+from typing import Iterator
+import os
+import jax
+
+jax.config.update("jax_compilation_cache_dir", "/tmp/jax_cache")
+jax.config.update("jax_persistent_cache_min_entry_size_bytes", -1)
+jax.config.update("jax_persistent_cache_min_compile_time_secs", 0)
+jax.config.update(
+    "jax_persistent_cache_enable_xla_caches", "xla_gpu_per_fusion_autotune_cache_dir"
+)
+jax.distributed.initialize()
 
 from parkiet.jax.audio import apply_audio_delay, build_delay_indices
 
